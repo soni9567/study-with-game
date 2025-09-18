34d4052efb83147760daf4d26685c5212e556e3d
soni9567 <shubhamsoni031031@gmail.com>
2025-09-18 21:01:32 +0530

    first test

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AnimatedPeriodicTable from './components/PeriodicTable'
+
+const App = () => {
+  return (
+    <div className=''><AnimatedPeriodicTable /></div>
+  )
+}
+
+export default App
